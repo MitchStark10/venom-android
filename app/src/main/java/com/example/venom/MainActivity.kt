b3bf7ecad641119df92b43b10869ee6e5b4ed789
Mitch Stark <mitchstark10@gmail.com>
2024-04-11 19:21:51 -0500

    edge to edge

--- app/src/main/java/com/example/venom/MainActivity.kt
+++ app/src/main/java/com/example/venom/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -27,6 +28,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         val sharedPrefs =
             applicationContext.getSharedPreferences("VENOM_SHARED_PREFS", MODE_PRIVATE)
+
+        enableEdgeToEdge()
 
         setContent {
             VenomTheme {
--- app/src/main/java/com/example/venom/layout/ListView.kt
+++ app/src/main/java/com/example/venom/layout/ListView.kt
@@ -1,22 +1,23 @@
 package com.example.venom.layout
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import com.example.venom.classes.GroupBy
 import com.example.venom.classes.List
 import com.example.venom.classes.Modal
 import com.example.venom.classes.SelectedView
 import com.example.venom.components.PageWithGroupedTasks
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun ListView(list: List) {
+
     Scaffold(
         floatingActionButton = {
             FloatingActionButton(onClick = { SelectedView.openModal = Modal.TASK_MODAL }) {
@@ -24,7 +25,7 @@ fun ListView(list: List) {
             }
         }
     ) {
-        Box(modifier = Modifier.padding(it)) {
+        Box {
             PageWithGroupedTasks(title = list.listName, tasks = list.tasks, groupBy = GroupBy.DATE)
         }
     }
