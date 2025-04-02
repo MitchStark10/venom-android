d160bd35cec4639600588431cb6117aa905e2781
Mitch Stark <mitchstark10@gmail.com>
2025-04-01 20:14:18 -0500

    automated release commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.venom.venomtasks"
         minSdk = 31
         targetSdk = 35
-        versionCode = 31
+        versionCode = 32
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
